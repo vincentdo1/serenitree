9690bfd4b28ab99b5dea563dedcb11f316d35f7b
Vincent Do <wavyfingers@gmail.com>
2024-02-24 11:19:14 -0600

    chore: set up server and initialize endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import user from './services/user'
+import plant from './services/plant'
+import spell from './services/spell'
+import reflection from './services/reflection'
+import quest from './services/quest'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => c.text('Hello Node.js!'))
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const routes = app.route('/api/user', user).route('/api/plant', plant).route('/api/spell', spell).route('/api/reflection', reflection).route('/api/quest', quest)
 
-serve({
-  fetch: app.fetch,
-  port
-})
+serve(app)
+
+export default app
+export type AppType = typeof routes
