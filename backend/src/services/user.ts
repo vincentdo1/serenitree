df178f39fe4eb1653e2651cd1c57364a59516d13
Vincent Do <wavyfingers@gmail.com>
2024-02-24 16:20:24 -0600

    feat: quest endpoints work as expected

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
+import { SQL, eq, inArray } from 'drizzle-orm'
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import { user } from '../schema';
+import connectDB from '../utils/connectDB';
 
 const app = new Hono()
 
-app.get('/', async (c) => {
-    return c.json([])
-  })
+app.get('/:userId', connectDB, async (c) => {
+  const param = await c.req.param('userId')
+
+  console.log(param)
+
+  return c.json(await c.get('db').select().from(user).where(eq(user.id, param)).execute());
+});
+
+app.post('/', connectDB, async (c) => {
+  const body = await c.req.json()
+
+  console.log(body)
+
+  return c.json(await c.get('db').insert(user).values({...body}).returning());
+});
 
 export default app
