d8150e810c30a9a762fff62811dd692e85148922
Vincent Do <wavyfingers@gmail.com>
2024-02-24 12:28:28 -0600

    chore: add basic endpoints, functionality

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async (c) => {
-    return c.json([])
-  })
+app.get('/:userId', async (c) => {
+    const params = c.req.param('userId')
+    console.log(params)
+    return c.json([]);
+});
+
+app.post('/:userId', async (c) => {
+    const params = c.req.param('userId')
+    console.log(params)
+    return c.json([]);
+});
+
+app.put('/:userId', async (c) => {
+    const params = c.req.param('userId')
+    console.log(params)
+    return c.json([]);
+});
+
+app.delete('/:userId', async (c) => {
+    const params = c.req.param('userId')
+    console.log(params)
+    return c.json([]);
+});
 
 export default app
